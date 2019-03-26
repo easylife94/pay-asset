cdea369afb7f05bfc2ecd57794105dd04fd373a2
chenwei <cw961590280@hotmail.com>
2019-03-26 10:12:14 +0800

    完善框架，可运行

--- pay-asset-web/src/main/java/com/pay/web/PayAssetWebApplication.java
+++ pay-asset-web/src/main/java/com/pay/web/PayAssetWebApplication.java
@@ -4,6 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = {"com.pay.*"})
 @MapperScan("com.pay.api.core.dao")
